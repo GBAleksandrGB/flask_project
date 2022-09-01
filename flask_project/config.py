3c58ceb007b53932954e436f2d58ae5721cca69c
GBAleksandrGB <tomilov.alex@gmail.com>
2022-09-01 22:54:38 +0300

    Initial commit

--- flask_project/config.py
+++ flask_project/config.py
@@ -1,10 +1,3 @@
-import os
-from boto.s3.connection import S3Connection
-
-
-s3 = S3Connection(os.environ['MAIL_PASSWORD'], os.environ['SECRET_KEY'])
-
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
