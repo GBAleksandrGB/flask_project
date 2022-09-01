2b6ec6dfd432e05ea9f04d463231007664a9de12
GBAleksandrGB <tomilov.alex@gmail.com>
2022-09-01 22:54:38 +0300

    Initial commit

--- flask_project/config.py
+++ flask_project/config.py
@@ -4,7 +4,7 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
-MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_PORT = int(os.environ.get('MAIL_PORT'))
 MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
