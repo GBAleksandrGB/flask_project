350668457d39d4a17b48a3eada8908ed72e042b1
GBAleksandrGB <tomilov.alex@gmail.com>
2022-09-01 22:54:38 +0300

    Initial commit

--- flask_project/config.py
+++ flask_project/config.py
@@ -1,12 +1,15 @@
 import os
 from boto.s3.connection import S3Connection
 
-s3 = S3Connection(os.environ['SECRET_KEY'], os.environ['MAIL_PASSWORD'])
+
+key_1 = S3Connection(os.environ['SECRET_KEY'])
+key_2 = S3Connection(os.environ['MAIL_PASSWORD'])
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+SECRET_KEY = key_1
 MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = "example@gmail.com"
+MAIL_PASSWORD = key_2
