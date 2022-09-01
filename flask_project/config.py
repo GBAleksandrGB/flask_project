6962121d497bad9492cd57213d3fb0b1a1dbada4
GBAleksandrGB <tomilov.alex@gmail.com>
2022-09-01 22:54:38 +0300

    Initial commit

--- flask_project/config.py
+++ flask_project/config.py
@@ -1,11 +1,10 @@
 import os
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
+MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
